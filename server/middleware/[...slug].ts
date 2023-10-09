b5bdf6a022029a46bf1b4b4b73245ba2103f69a9
Hao <tranhuuhao1407@gmail.com>
2023-10-09 22:02:12 +0700

    add
    add

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -8,7 +8,7 @@
         next();
     }
 };
- */
+*/
 export default defineEventHandler((event) => {
     const headers = event.req.headers;
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
